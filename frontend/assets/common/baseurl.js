0741183298bce1623f00d766d8be60f33a1fde1c
reden21345 <mirandilla395@gmail.com>
2024-07-30 20:06:04 +0800

    baseURL frontend

--- frontend/assets/common/baseurl.js
+++ frontend/assets/common/baseurl.js
@@ -4,8 +4,8 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.1.9:3001/api/'
-: baseURL = 'http://192.168.1.9:3001/api/'
+? baseURL = 'https://petmobile.onrender.com/api/'
+: baseURL = 'https://petmobile.onrender.com/api/'
 }
 
 export default baseURL;
